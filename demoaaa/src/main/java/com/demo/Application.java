2f8346ba494b94aa84759de7627d9a36a72f6e9c
tengkang <862953754@qq.com>
2023-09-11 04:20:00 +0800

    第一次提交

---
+++ demoaaa/src/main/java/com/demo/Application.java
@@ -0,0 +1,16 @@
+package com.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author tengkang
+ * @date 2023/9/11 3:47
+ * @desc please describe this project simply
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
---
+++ demobbb/src/main/java/com/demo/bbb/controller/DemoController.java
@@ -0,0 +1,23 @@
+package com.demo.bbb.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author tengkang
+ * @date 2023/9/11 3:55
+ * @desc please describe this project simply
+ */
+@RestController
+@RequestMapping("/demo")
+public class DemoController {
+
+    @GetMapping("/aaa")
+    public String aaa() {
+        return "aaaaaaaa";
+    }
+
+
+
+}
